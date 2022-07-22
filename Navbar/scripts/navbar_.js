4ba9f1b5aac1169e11dc0d08b135159bc8e00aa1
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-07-22 12:28:01 -0700

    navbar

---
+++ Navbar/scripts/navbar_.js
@@ -0,0 +1,51 @@
+// let navbar = () => {
+ 
+//     return ` <div id="left_side">
+//     <div id="logo"><a href="# ">
+//     <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/monday-logo-x2.png">
+//     </a>
+//     </div>
+   
+//     <div id="left_menu">
+       
+//         <div id="products" class="left_subdiv"> <p class="left_title"> Products </p> 
+
+//             <div id="drop_down1">
+          
+//             </div>
+//         </div>
+//         <div id="use_cases" class="left_subdiv">  <p class="left_title"> Use cases </p> 
+
+
+//             <div id="drop_down2">
+          
+//             </div>
+//         </div>
+//         <div id="features" class="left_subdiv"> <p class="left_title"> Features  </p> 
+            
+//             <div id="drop_down3">
+          
+//             </div>
+//         </div>
+//         <div id="resources" class="left_subdiv"> <p class="left_title"> Resources  </p> 
+            
+//             <div id="drop_down4">
+          
+//             </div>
+//         </div>
+//     </div>
+// </div>
+//     <div id="right_menu">
+//         <div><a href=""> <p class="right_title">  Pricing </p> </a></div>
+//         <div><a href="#"> <p class="right_title"> Contact sales </p> </a></div>
+//         <div><a href="#"> <p class="right_title"> Log in </p>  </a></div>
+//         <button id="btn_getStrted"><a href="#">Get Started -></a></button>
+//     </div>`
+// }
+
+
+
+// export default navbar;
+
+
+
