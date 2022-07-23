9e3c150e37f189340d86789cb7bf6586f4b28e47
aasimsyed97 <sayedaasim555@gmail.com>
2022-07-23 23:43:05 +0530

    final push

---
+++ HOME_PAGE/Homepage/homepage.js
@@ -0,0 +1,5 @@
+// import navbar from "../day3/navbar.js"
+//  document.getElementById("navbar").innerHTML =navbar()
+
+import footer from "../footer/footer.js"
+ document.getElementById("footer").innerHTML = footer()
