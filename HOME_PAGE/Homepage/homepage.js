58dbd49ba48dc91e1731e769737952ef24206a8b
Shivaprasad-sBhat <bhatshivaprasad98@gmail.com>
2022-07-24 09:52:34 -0700

    navbar

--- HOME_PAGE/Homepage/homepage.js
+++
@@ -1,5 +0,0 @@
-// import navbar from "../day3/navbar.js"
-//  document.getElementById("navbar").innerHTML =navbar()
-
-import footer from "../footer/footer.js"
- document.getElementById("footer").innerHTML = footer()
